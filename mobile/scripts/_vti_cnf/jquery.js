3e5bf8b430e7db66da530324bcd33061dfc7e098
Rodrigo Caja <rodrigoccaja@gmail.com>
2012-12-21 11:12:04 -0200

    Added site initial files and codes

---
+++ mobile/scripts/_vti_cnf/jquery.js
@@ -0,0 +1,8 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|17 Dec 2012 13:59:43 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_cacheddtm:TX|17 Dec 2012 13:59:43 -0000
+vti_filesize:IR|267739
+vti_modifiedby:SR|RODRIGO-XPS\\Rodrigo
+vti_nexttolasttimemodified:TR|30 Nov 2012 01:53:16 -0000
+vti_backlinkinfo:VX|
---
+++ scripts/_vti_cnf/mobile_redirect_service.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|RODRIGO-XPS\\Rodrigo
+vti_modifiedby:SR|RODRIGO-XPS\\Rodrigo
+vti_timelastmodified:TR|21 Dec 2012 11:45:41 -0000
+vti_timecreated:TR|21 Dec 2012 11:45:41 -0000
+vti_cacheddtm:TX|21 Dec 2012 11:45:41 -0000
+vti_filesize:IR|2113
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|index.html
